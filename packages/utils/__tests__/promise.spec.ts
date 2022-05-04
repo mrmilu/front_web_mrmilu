9575e630ec596dc93cbf92b28e4ed1ff501a41bc
Guido Ceraso <guidoceraso@gmail.com>
2022-05-04 18:40:20 +0200

    chore(utils): add tests to promise module

---
+++ packages/utils/__tests__/promise.spec.ts
@@ -0,0 +1,48 @@
+import { makeCancelable, timeout } from "../src";
+
+jest.useFakeTimers();
+jest.spyOn(global, "setTimeout");
+
+describe("timeout promise", () => {
+  test("should run promise timer for 1 second", () => {
+    const pendingPromise = timeout(1000).then(() => {
+      expect(setTimeout).toHaveBeenCalledTimes(1);
+      expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+    });
+    jest.runAllTimers();
+    return pendingPromise;
+  });
+});
+
+describe("cancelable promise", () => {
+  test("should cancel promise then chain", () => {
+    const onCancelCallback = jest.fn();
+    const testPromise = () => {
+      const timeoutPromise = timeout(2000);
+      const cancelablePromise = makeCancelable(timeoutPromise);
+      cancelablePromise.onCancel(onCancelCallback);
+      cancelablePromise.cancel();
+      jest.runAllTimers();
+      return timeoutPromise;
+    };
+
+    return testPromise().then(() => {
+      expect(onCancelCallback).toHaveBeenCalled();
+    });
+  });
+
+  test("should not cancel promise then chain", () => {
+    const onCancelCallback = jest.fn();
+    const testPromise = () => {
+      const timeoutPromise = timeout(2000);
+      const cancelablePromise = makeCancelable(timeoutPromise);
+      cancelablePromise.onCancel(onCancelCallback);
+      jest.runAllTimers();
+      return timeoutPromise;
+    };
+
+    return testPromise().then(() => {
+      expect(onCancelCallback).not.toHaveBeenCalled();
+    });
+  });
+});
