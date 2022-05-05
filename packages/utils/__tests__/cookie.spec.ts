ae38026ca6f4abb2348458f5d9d28f8bb5cea875
Guido Ceraso <guidoceraso@gmail.com>
2022-05-05 09:09:05 +0200

    chore(root): changed test for it for a more spec test writing

--- packages/utils/__tests__/cookie.spec.ts
+++ packages/utils/__tests__/cookie.spec.ts
@@ -9,26 +9,26 @@ describe("CookieUtils", () => {
       });
     });
 
-    test("should get cookie value and be one", () => {
+    it("should get cookie value and be one", () => {
       const expected = "one";
       const result = CookieUtils.getCookie("example1");
 
       expect(result).toBe(expected);
     });
-    test("should get cookie value and be null", () => {
+    it("should get cookie value and be null", () => {
       const result = CookieUtils.getCookie("example3");
 
       expect(result).toBeNull();
     });
   });
 
-  test("setCookie should set cookie in document", () => {
+  it("setCookie should set cookie in document", () => {
     const expected = "customCookie=customValue";
     CookieUtils.setCookie("customCookie", "customValue");
     expect(document.cookie).toInclude(expected);
   });
 
-  test("eraseCookie should set cookie in document", () => {
+  it("eraseCookie should set cookie in document", () => {
     const value = "customCookie";
     const staticValue = "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
     const expected = value + staticValue;
@@ -38,7 +38,7 @@ describe("CookieUtils", () => {
     expect(document.cookie).toBe(expected);
   });
 
-  test("cookieExpiration should get Date with corresponding expiration", () => {
+  it("cookieExpiration should get Date with corresponding expiration", () => {
     const dateExpirationYear = new Date().getFullYear() + 1;
     const yearExpiration = CookieUtils.cookieExpirationDate();
     expect(yearExpiration.getFullYear()).toEqual(dateExpirationYear);
--- packages/utils/__tests__/promise.spec.ts
+++ packages/utils/__tests__/promise.spec.ts
@@ -4,7 +4,7 @@ jest.useFakeTimers();
 jest.spyOn(global, "setTimeout");
 
 describe("timeout promise", () => {
-  test("should run promise timer for 1 second", () => {
+  it("should run promise timer for 1 second", () => {
     const pendingPromise = timeout(1000).then(() => {
       expect(setTimeout).toHaveBeenCalledTimes(1);
       expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
@@ -15,7 +15,7 @@ describe("timeout promise", () => {
 });
 
 describe("cancelable promise", () => {
-  test("should cancel promise then chain", () => {
+  it("should cancel promise then chain", () => {
     const onCancelCallback = jest.fn();
     const testPromise = () => {
       const timeoutPromise = timeout(2000);
@@ -31,7 +31,7 @@ describe("cancelable promise", () => {
     });
   });
 
-  test("should not cancel promise then chain", () => {
+  it("should not cancel promise then chain", () => {
     const onCancelCallback = jest.fn();
     const testPromise = () => {
       const timeoutPromise = timeout(2000);
