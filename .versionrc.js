f2954b9a859f05dcf2fcded62fdbb3126e2da57b
Guido Ceraso <guidoceraso@gmail.com>
2022-05-10 09:56:46 +0200

    chore(root): force repo url

--- .versionrc.js
+++ .versionrc.js
@@ -17,5 +17,7 @@ module.exports = {
   packageFiles: packageDirectories.map((packageDir) => ({
     filename: `${PACKAGES_ROOT_DIR}/${packageDir}/package.json`,
     type: "json"
-  }))
+  })),
+  commitUrlFormat: "https://github.com/mrmilu/front_web_mrmilu/commit/{{hash}}",
+  compareUrlFormat: "https://github.com/mrmilu/front_web_mrmilu/compare/{{previousTag}}...{{currentTag}}"
 };
