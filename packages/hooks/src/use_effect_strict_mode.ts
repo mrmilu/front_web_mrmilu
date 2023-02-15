6297ba20e64b98433f58c4bd9a1ae88ece653263
Arnau Gómez <agomez@mrmilu.com>
2023-02-15 09:55:22 +0100

    feat(hooks): create useEffectRunOnce hook instead of modifying useEffectStrictMode hook

--- packages/hooks/src/use_effect_strict_mode.ts
+++ packages/hooks/src/use_effect_strict_mode.ts
@@ -1,19 +1,34 @@
-import type { DependencyList, EffectCallback } from "react";
+import type { EffectCallback } from "react";
 import { useEffect, useRef } from "react";
 
 /**
  * useEffect hook for React 18 in strict mode that executes only ones in development mode
  * @param effect
- * @param deps
  */
-const useEffectStrictMode = (effect: EffectCallback, deps: DependencyList) => {
-  const runOnce = useRef(false);
+const useEffectStrictMode = (effect: EffectCallback) => {
+  const runEffectOnce = useRef(false);
+  const runCleanupOnce = useRef(false);
+  const cleanupDestructor = useRef<ReturnType<EffectCallback> | null>(null);
 
   useEffect(() => {
-    if (runOnce.current || process.env.NODE_ENV === "production") return effect();
-    runOnce.current = true;
+    if (!runEffectOnce.current) {
+      runEffectOnce.current = true;
+      const destructor = effect();
+      if (destructor) cleanupDestructor.current = destructor;
+    }
+
+    return () => {
+      if (process.env.NODE_ENV === "production" && cleanupDestructor.current) {
+        cleanupDestructor.current();
+      } else {
+        if (runCleanupOnce.current && cleanupDestructor.current) {
+          cleanupDestructor.current();
+        }
+        runCleanupOnce.current = true;
+      }
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
+  }, []);
 };
 
 export default useEffectStrictMode;
