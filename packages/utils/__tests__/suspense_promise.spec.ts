bd3a984b04c71193f13d72e9e3261e1b131439a5
Guido Ceraso <guidoceraso@gmail.com>
2023-03-29 16:02:19 +0200

    chore(utils): added suspense_promise tests

---
+++ packages/utils/__tests__/suspense_promise.spec.ts
@@ -0,0 +1,51 @@
+import { timeout, wrapPromise, wrapPromisePending } from "../src";
+import * as util from "util";
+
+describe("suspense promise", () => {
+  describe("wrap promise", () => {
+    beforeAll(() => {
+      jest.useFakeTimers();
+    });
+    it("should wrap promise and read correct resolve value", async () => {
+      const wrappedPromise = await wrapPromise(Promise.resolve("foo"));
+      const readValue = wrappedPromise.read();
+      expect(readValue).toBe("foo");
+    });
+
+    it("should wrap promise and reject on read", async () => {
+      const wrappedPromise = await wrapPromise(Promise.reject("bar"));
+      try {
+        wrappedPromise.read();
+      } catch (e) {
+        expect(e).toBe("bar");
+      }
+    });
+
+    it("should wrap promise and keep pending", async () => {
+      const fakePromise = async () => {
+        await timeout(10000);
+        return "baz";
+      };
+      try {
+        const wrappedPromise = await wrapPromise(fakePromise());
+        wrappedPromise.read();
+      } catch (e) {
+        expect(util.inspect(e)).toInclude("pending");
+      }
+    });
+    afterAll(() => {
+      jest.runAllTimers();
+    });
+  });
+
+  describe("wrap pending promise", () => {
+    it("should wrap promise and keep pending", async () => {
+      try {
+        const wrappedPromise = await wrapPromisePending();
+        wrappedPromise.read();
+      } catch (e) {
+        expect(util.inspect(e)).toInclude("undefined");
+      }
+    });
+  });
+});
